8df289b22125ca2ff4598ff6dfc9d8d8c37c90cc
LEE HANBIN <hanbin@kakao.com>
2020-12-27 22:26:46 +0900

    update 3-1.py

--- python-for-coding-test/ch03/3-1.py
+++ python-for-coding-test/ch03/3-1.py
@@ -1,10 +1,7 @@
-def solution(N):
-    coins = [500, 100, 50, 10]
-    answer = 0
-    for coin in coins:
-        answer += N // coin
-        N %= coin
-    return answer
-
-
-print(solution(1260))
+N = int(input(">> "))
+coins = [500, 100, 50, 10]
+answer = 0
+for coin in coins:
+    answer += N // coin
+    N %= coin
+print(answer)
