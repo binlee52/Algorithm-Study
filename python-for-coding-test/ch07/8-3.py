06f597cbf261a05754379bb34e462b9d63346177
LEE HANBIN <hanbin@kakao.com>
2020-12-30 04:15:19 +0900

    update 8-3.py

---
+++ python-for-coding-test/ch07/8-3.py
@@ -0,0 +1,12 @@
+n = int(input())
+k = list(map(int, input().split()))
+d = [0] * 100
+
+# Dynamic Programming(Bottom Up)
+d[0] = k[0]
+d[1] = max(d[0], d[1])
+
+for i in range(n):
+    d[i] = max(d[i-1], d[i-2] + k[i])
+
+print(d[n-1])
