0a261c7f73b31e930d0305443a7925122d255a21
LEE HANBIN <hanbin@kakao.com>
2020-12-30 01:34:17 +0900

    update 7-2.py

---
+++ python-for-coding-test/ch07/7-2.py
@@ -0,0 +1,23 @@
+# 이진 탐색 코드(재귀함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+
+    mid = (start + end)//2
+    if array[mid] < target:
+        return binary_search(array, target, mid + 1, end)
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return mid
+
+
+n, target = map(int, input().split())
+array = list(map(int, input().split()))
+array.sort()
+result = binary_search(array, target, 0, len(array)-1)
+
+if result:
+    print(result + 1)
+else:
+    print("{}이 존재하지 않습니다.".format(target))
