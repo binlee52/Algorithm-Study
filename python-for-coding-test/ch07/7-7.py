2de830b420e7efb8384653498148d14cbee0540c
LEE HANBIN <hanbin@kakao.com>
2020-12-30 02:11:52 +0900

    update 7-7.py

--- python-for-coding-test/ch07/7-7.py
+++ python-for-coding-test/ch07/7-7.py
@@ -1,18 +1,12 @@
-import bisect as bs
-
 n = int(input())        # 부품 N개
 array = list(map(int, input().split()))     # 부품 번호
 m = int(input())        # 찾으려는 부품 개수
 x = list(map(int, input().split()))     # 찾으려는 부품 번호
 
-array.sort()
-x.sort()
-
+array = set(array)
 for target in x:
-    result = bs.bisect_left(array, target)
-    if array[result] == target:
-        print("yes", end=' ')
+    if target in array:
+        print('yes', end=' ')
     else:
-        print("no", end=' ')
-
+        print('no', end=' ')
 
