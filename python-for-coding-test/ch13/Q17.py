7d36833f221bb3032b3ef9cba26304063ca236f2
LEE HANBIN <hanbin@kakao.com>
2021-01-25 15:28:50 +0900

    update Q17.py

--- python-for-coding-test/ch13/Q17.py
+++ python-for-coding-test/ch13/Q17.py
@@ -1,4 +1,4 @@
-import heapq as hq
+from collections import deque
 import sys
 
 input = sys.stdin.readline
@@ -8,28 +8,29 @@ for _ in range(N):
     data.append(list(map(int, input().split())))
 S, X, Y = map(int, input().split())
 
+q = []
+for i in range(N):
+    for j in range(N):
+        if data[i][j] != 0:
+            q.append((data[i][j], i, j, 0))
+q.sort()
+q = deque(q)
+
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-q = []
+while q:
+    virus, x, y, time = q.popleft()
+    if time == S:
+        break
 
-for _ in range(S):
-    for i in range(N):
-        for j in range(N):
-            if data[i][j] != 0:
-                hq.heappush(q, (data[i][j], i, j))
-    while q:
-        virus, x, y = hq.heappop(q)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if data[nx][ny] == 0:
-                    data[nx][ny] = virus
-    print("graph")
-    for row in data:
-        for col in row:
-            print(col, end=' ')
-        print()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N:
+            if data[nx][ny] == 0:
+                data[nx][ny] = virus
+                q.append((data[nx][ny], nx, ny, time+1))
+
 
 print(data[X-1][Y-1])
