0b5d57731bddf226952b973df3724615cfa00465
LEE HANBIN <hanbin@kakao.com>
2021-04-07 16:21:48 +0900

    update 4-1.py

--- python-for-coding-test/ch04/4-1.py
+++ python-for-coding-test/ch04/4-1.py
@@ -1,13 +1,13 @@
 n = int(input())
-data = [[False] * (n+1) for _ in range(n+1)]
-move = {'L': (0, -1), 'R': (0, 1), 'U': (-1, 0), 'D': (1, 0)}
-
-x, y = 1, 1
 actions = input().split()
+move = {'L': (0, -1), 'R': (0, 1), 'U': (-1, 0), 'D': (1, 0)}
+x, y = 1, 1
 for action in actions:
     nx = x + move[action][0]
     ny = y + move[action][1]
-    if 1 <= nx <= n and 1 <= ny <= n:
+
+    if 0 < nx <= n and 0 < ny <= n:
         x, y = nx, ny
 
-print(x ,y)
+print(x, y)
+
