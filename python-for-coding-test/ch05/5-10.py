c2acdccd3d8324143405b270b7194bdb82a6ac0f
LEE HANBIN <hanbin@kakao.com>
2021-04-08 18:37:42 +0900

    review 5-10.py

--- python-for-coding-test/ch05/5-10.py
+++ python-for-coding-test/ch05/5-10.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 n, m = map(int, input().split())
 graph = []
 for _ in range(n):
@@ -7,7 +8,6 @@ for _ in range(n):
 # 상, 하, 좌, 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 
 answer = 0
 
@@ -20,7 +20,7 @@ for i in range(n):
             q = deque([(i, j)])     # init
             # 방문 횟수 ++
             answer += 1
-            
+
             # q가 빌 때까지
             while q:
                 x, y = q.popleft()
@@ -31,6 +31,5 @@ for i in range(n):
                     # nx, ny가 범위 안에 있고, 방문하지 않았을 때
                     if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != 1:
                         q.append((nx, ny))      # 큐에 넣는다
-
 
 print(answer)
