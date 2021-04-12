6e533b977b00140d65c933027ac3695ff0a43dc9
LEE HANBIN <hanbin@kakao.com>
2021-04-12 21:59:12 +0900

    review ch07

--- python-for-coding-test/ch07/7-2.py
+++ python-for-coding-test/ch07/7-2.py
@@ -1,15 +1,26 @@
-# 이진 탐색 코드(재귀함수)
+# # 이진 탐색 코드(재귀함수)
+# def binary_search(array, target, start, end):
+#     if start > end:
+#         return None
+#
+#     mid = (start + end)//2
+#     if array[mid] < target:
+#         return binary_search(array, target, mid + 1, end)
+#     elif array[mid] > target:
+#         return binary_search(array, target, start, mid - 1)
+#     else:
+#         return mid
+
 def binary_search(array, target, start, end):
     if start > end:
         return None
-
     mid = (start + end)//2
-    if array[mid] < target:
-        return binary_search(array, target, mid + 1, end)
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
-    else:
+    if array[mid] == target:
         return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
 
 
 n, target = map(int, input().split())
--- python-for-coding-test/ch07/7-8.py
+++ python-for-coding-test/ch07/7-8.py
@@ -21,28 +21,3 @@ while start <= end:
         start = mid + 1
 
 print(result)
-
-
-# # 재귀함수 (내가한거)
-# def binary_search(array, target, start, end, max_height):
-#     if start > end:
-#         return max_height
-#
-#     height = (start + end) // 2     # 절단기 높이
-#     length = 0  # 손님에게 주는 떡의 길이
-#     for a in array:
-#         piece = a - height
-#         if piece > 0:
-#             length += piece
-#
-#     # 조건을 만족할 시
-#     if length > target:
-#         return binary_search(array, target, height + 1, end, height)
-#     elif length < target:
-#         return binary_search(array, target, start, height - 1, max_height)
-#     else:
-#         return height
-#
-#
-
-# print(binary_search(array, m, 0, array[-1], 0))
