b9828cd4b7c4d32dcccbf1c64f280403b713323b
LEE HANBIN <hanbin@kakao.com>
2021-04-20 05:35:06 +0900

    update BOJ16681.py
    70%까진 맞음..

--- BOJ/Dijkstra/BOJ16681.py
+++ BOJ/Dijkstra/BOJ16681.py
@@ -16,19 +16,13 @@ for _ in range(M):
     graph[a].append((n, b))     # (거리, 지점)
     graph[b].append((n, a))     # (거리, 지점)
 
-# 올라갈 때 거리
-up_distance = [INF] * (N+1)
-up_distance[1] = 0     # start
 
-# 내려갈 때 거리(N에서 올라갈 때 거리)
-down_distance = [INF] * (N+1)
-down_distance[N] = 0    # finish
+def dijkstra(start):
+    distance = [INF] * (N+1)
+    distance[start] = 0
 
-
-def dijkstra(start, distance):
     q = []
-    heapq.heappush(q, (0, 1))      # (거리, 도착지)
-
+    heapq.heappush(q, (0, start))      # (거리, 도착지)
     while q:
         dist, now = heapq.heappop(q)
         # 최단거리로 이동한 것이 아니라면
@@ -39,28 +33,27 @@ def dijkstra(start, distance):
         # i[0] : 거리, i[1] : 도착지
         for i in graph[now]:
             cost = dist + i[0]
+            # 기록된 도착지까지의 최단거리보다 이동거리가 짧아야 한다.
+            # 도착지의 높이가 현재 높이보다 높아야한다.
             if cost < distance[i[1]] and height[now] < height[i[1]]:
                 distance[i[1]] = cost
                 heapq.heappush(q, (cost, i[1]))
-# he - Dl
+    return distance
 
-dijkstra(1, up_distance)
-dijkstra(N, down_distance)
 
-move = []
+up_distance = dijkstra(1)
+down_distance = dijkstra(N)
+
+lst = []
 # 거쳐가는 지점 2~N-1 까지
 for i in range(2, N):
-    # 1과 N에서 모두 갈 수 있는 지점 i를 구한다.
+    # # 1과 N에서 모두 갈 수 있는 지점 i를 구한다.
     if INF not in (up_distance[i], down_distance[i]):
-        move.append((up_distance[i] + down_distance[i], i))     # (이동거리, 거쳐가는 지점)
-t = [(height[i[1]], i[1]) for i in move]
-result = []
-for a, b in zip(t, move):
-    result.append((a[0]-2) * E - D * b[0])
-print(result)
+        move = up_distance[i] + down_distance[i]
+        lst.append(height[i] * E - move*D)
 
-if result:
-    print(max(result))
+if lst:
+    print(max(lst))
 else:
     print("Impossible")
 
