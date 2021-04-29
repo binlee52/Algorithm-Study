71015f9b8a445bf8bb3bf597b79d5389e22f418a
LEE HANBIN <hanbin@kakao.com>
2021-04-29 17:11:45 +0900

    create BOJ2252.py

---
+++ BOJ/Topological sorting/BOJ2252.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+
+def topology_sort():
+    result = []
+    q = deque([])
+
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        result.append(now)
+        for x in graph[now]:
+            indegree[x] -= 1
+            if indegree[x] == 0:
+                q.append(x)
+
+    for x in result:
+        print(x, end=" ")
+
+
+topology_sort()
