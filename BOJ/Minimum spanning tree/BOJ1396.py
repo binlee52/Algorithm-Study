0313e821d5d11269a834fa54a97aaf763ff6d66d
LEE HANBIN <hanbin@kakao.com>
2021-06-06 00:58:57 +0900

    update BOJ1396.py
    FAIL

--- BOJ/Minimum spanning tree/BOJ1396.py
+++ BOJ/Minimum spanning tree/BOJ1396.py
@@ -1,6 +1,8 @@
 import sys
 from collections import deque
+import copy
 
+INF = int(1e12)
 input = sys.stdin.readline
 
 def find_parent(parent, x):
@@ -18,65 +20,39 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 
-
-
 # n : 정점의 개수, m : 간선의 개수
 n, m = map(int, input().split())
 
-edges = []
 result = 0
 
+graph = []
 for _ in range(m):
     a, b, c = map(int, input().split())
-    edges.append((c, a, b))     # 간선, 정점, 정점
+    graph.append((c, a, b))
 
-# 간선 정렬
-edges.sort()
-
-parent = [i for i in range(n + 1)]
-graph = [[] for _ in range(n + 1)]
-
-for edge in edges:
-    cost, a, b = edge
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        graph[a].append((cost, b))  # 비용, 도착지
-        graph[b].append((cost, a))
+graph.sort()
+graph = deque(graph)
 
 Q = int(input())
-
 for _ in range(Q):
+    tmp = 0
+    q = copy.deepcopy(graph)
+    parent = [i for i in range(n+1)]
     x, y = map(int, input().split())
-    u = 0      # 최소온도
-    visited = [0] * (n + 1)     # 방문 여부 확인
 
-    q = deque([(0, x)])    # 비용, 도착지
+    u = INF
     while q:
-        dist, now = q.popleft()
+        c, a, b = q.popleft()
 
-        # 방문한 적이 있으면
-        if visited[now]:
-            continue
-        
-        # 방문하기까지의 비용
-        visited[now] = max(visited[now], dist)
-        
-        # y에 도착했을 시
-        if now == y:
-            u = dist
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
 
-        # i[0] : 비용, i[1] : 도착지
-        for i in graph[now]:
-            cost = max(i[0], dist)
-            q.append((cost, i[1]))
+        if find_parent(parent, x) == find_parent(parent, y):
+            u = c
+            break
 
-    # x에서 y까지 길이 존재할 때
-    visited[x] = 1
-    if u:
-        v = 0
-        for i in visited:
-            if 0 < i <= u:
-                v += 1
+    if u != INF:
+        v = len([find_parent(parent, i) for i in parent if find_parent(parent, i) == find_parent(parent, x)])
         print(u, v)
     else:
         print(-1)
