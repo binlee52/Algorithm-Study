ab06bd796b4191031765a695e7860984a3f7989e
LEE HANBIN <hanbin@kakao.com>
2021-07-06 17:55:49 +0900

    create [카카오 인턴] 경주로 건설.py

---
+++ Programmers/2020 카카오 인턴십/[카카오 인턴] 경주로 건설.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+from collections import deque
+
+
+def solution(board):
+    answer = min(dijkstra(board, 1), dijkstra(board, 2))
+    return answer
+
+
+def dijkstra(graph, d):
+    N = len(graph)
+    INF = sys.maxsize
+    distance = [[INF] * N for _ in range(N)]
+    distance[0][0] = 0
+
+    q = deque([(0, 0, 0, d)])
+    # 북동남서
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    while q:
+        dist, x, y, d = q.popleft()
+        if distance[x][y] < dist:
+            continue
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 다음 것에서 바라보는 방향
+            # 범위 안일 때
+            if 0 <= nx < N and 0 <= ny < N and not graph[nx][ny]:
+                # 도로로 연결이 가능할 때
+                cost = dist + 100 if i == d else dist + 600
+                # 현재까지의 최소비용보다 작다면
+                if distance[nx][ny] > cost:
+                    distance[nx][ny] = cost
+                    q.append((cost, nx, ny, i))
+    return distance[-1][-1]
